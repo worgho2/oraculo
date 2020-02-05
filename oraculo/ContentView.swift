6cdc4e78fa61b91b711f88b463e6e5eafbb4794d
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2020-02-05 12:29:01 -0300

    Initial Commit

---
+++ oraculo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  oraculo
+//
+//  Created by otavio on 05/02/20.
+//  Copyright © 2020 Raieiros Studio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
